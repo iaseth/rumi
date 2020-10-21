c7e28a86f11ceb7661e27ae9cad4c489cc622061
Ankur Seth <iankurseth@gmail.com>
2020-10-21 21:59:44 +0530

    Added rumi_new and rumi_delete declarations

--- include/rumi_base.h
+++ include/rumi_base.h
@@ -1,7 +1,11 @@
 #ifndef RUMI_BASE_H
 #define RUMI_BASE_H
 
+#include "rumi_types.h"
 
+Rumi rumi_new (char *title);
+Rumi rumi_new_child (Rumi rumi);
+Rumi rumi_delete (Rumi rumi);
 
 
 
--- lib/rumi_base.c
+++ lib/rumi_base.c
@@ -1,5 +1,23 @@
 #include "rumi_base.h"
 
+#include <stdlib.h>
 
+Rumi
+rumi_new (char *title)
+{
+	return NULL;
+}
+
+Rumi
+rumi_new_child (Rumi rumi)
+{
+	return NULL;
+}
+
+Rumi
+rumi_delete (Rumi rumi)
+{
+	return NULL;
+}
 
 
