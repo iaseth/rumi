aa81c5750eb0f83b9869ff3701b9fe13bf8fad61
Ankur Seth <iankurseth@gmail.com>
2020-10-22 16:08:50 +0530

    Added macro: RUMI_DEFINE_INDICATORS

--- lib/rumi_switches.c
+++ lib/rumi_switches.c
@@ -6,8 +6,11 @@
 	void rumi_turn_off_ ## x (Rumi rumi) { rumi->show_ ## x = false; } \
 	void rumi_turn_on_  ## x (Rumi rumi) { rumi->show_ ## x = true; }
 
+#define RUMI_DEFINE_INDICATORS(x) \
+	bool rumi_ ## x ## _is_off (Rumi rumi) { return !rumi->show_ ## x; } \
+	bool rumi_ ## x ## _is_on  (Rumi rumi) { return  rumi->show_ ## x; }
+
 RUMI_DEFINE_SWITCHES(debug);
-bool rumi_debug_is_off (Rumi rumi) { return !rumi->show_debug; }
-bool rumi_debug_is_on  (Rumi rumi) { return rumi->show_debug; }
+RUMI_DEFINE_INDICATORS(debug);
 
 
