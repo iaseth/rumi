df8d8ad4b4f063d9d75198409970758fdcae1b79
Ankur Seth <iankurseth@gmail.com>
2020-10-22 16:05:05 +0530

    Added macro: RUMI_DEFINE_SWITCHES

--- lib/rumi_switches.c
+++ lib/rumi_switches.c
@@ -1,8 +1,12 @@
 #include "rumi_switches.h"
 
-void rumi_turn_off_debug (Rumi rumi) { rumi->show_debug = false; }
-void rumi_turn_on_debug  (Rumi rumi) { rumi->show_debug = true; }
 
+
+#define RUMI_DEFINE_SWITCHES(x) \
+	void rumi_turn_off_ ## x (Rumi rumi) { rumi->show_ ## x = false; } \
+	void rumi_turn_on_  ## x (Rumi rumi) { rumi->show_ ## x = true; }
+
+RUMI_DEFINE_SWITCHES(debug);
 bool rumi_debug_is_off (Rumi rumi) { return !rumi->show_debug; }
 bool rumi_debug_is_on  (Rumi rumi) { return rumi->show_debug; }
 
