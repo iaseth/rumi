db1b6d6d6ec3460fa3f435690cffc36ac6526227
Ankur Seth <iankurseth@gmail.com>
2020-10-22 15:38:02 +0530

    Removed defs from header

--- include/rumi_switches.h
+++ include/rumi_switches.h
@@ -4,11 +4,11 @@
 #include "rumi_types.h"
 
 
-void rumi_turn_off_debug (Rumi rumi) { rumi->show_debug = false; }
-void rumi_turn_on_debug  (Rumi rumi) { rumi->show_debug = true; }
+void rumi_turn_off_debug (Rumi rumi);
+void rumi_turn_on_debug  (Rumi rumi);
 
-bool rumi_debug_is_off (Rumi rumi) { return !rumi->show_debug; }
-bool rumi_debug_is_on  (Rumi rumi) { return rumi->show_debug; }
+bool rumi_debug_is_off (Rumi rumi);
+bool rumi_debug_is_on  (Rumi rumi);
 
 
 
--- lib/rumi_switches.c
+++ lib/rumi_switches.c
@@ -1,5 +1,9 @@
 #include "rumi_switches.h"
 
+void rumi_turn_off_debug (Rumi rumi) { rumi->show_debug = false; }
+void rumi_turn_on_debug  (Rumi rumi) { rumi->show_debug = true; }
 
+bool rumi_debug_is_off (Rumi rumi) { return !rumi->show_debug; }
+bool rumi_debug_is_on  (Rumi rumi) { return rumi->show_debug; }
 
 
