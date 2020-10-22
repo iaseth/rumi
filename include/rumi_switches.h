81e52a68a2ed93169e6ee5c8ad093ad8e7a6496e
Ankur Seth <iankurseth@gmail.com>
2020-10-22 15:27:59 +0530

    Added switches and indicators for debug

--- include/rumi_switches.h
+++ include/rumi_switches.h
@@ -1,7 +1,14 @@
 #ifndef RUMI_SWITCHES_H
 #define RUMI_SWITCHES_H
 
+#include "rumi_types.h"
 
+
+void rumi_turn_off_debug (Rumi rumi) { rumi->show_debug = false; }
+void rumi_turn_on_debug  (Rumi rumi) { rumi->show_debug = true; }
+
+bool rumi_debug_is_off (Rumi rumi) { return !rumi->show_debug; }
+bool rumi_debug_is_on  (Rumi rumi) { return rumi->show_debug; }
 
 
 
