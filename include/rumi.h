c6e5658a3172974565d1359808eb9b4fd32fc8f8
Ankur Seth <iankurseth@gmail.com>
2020-10-22 18:48:43 +0530

    Made colors extern

--- include/rumi.h
+++ include/rumi.h
@@ -3,6 +3,7 @@
 
 #include "rumi_macros.h"
 #include "rumi_types.h"
+#include "rumi_colors.h"
 #include "rumi_base.h"
 #include "rumi_switches.h"
 #include "rumi_log.h"
--- include/rumi_colors.h
+++ include/rumi_colors.h
@@ -3,31 +3,31 @@
 
 #include "rumi_types.h"
 
-RumiColor rumi_color_reset = "\x1b[0m";
-RumiColor rumi_color_bright = "\x1b[1m";
-RumiColor rumi_color_dim = "\x1b[2m";
-RumiColor rumi_color_underscore = "\x1b[4m";
-RumiColor rumi_color_blink = "\x1b[5m";
-RumiColor rumi_color_reverse = "\x1b[7m";
-RumiColor rumi_color_hidden = "\x1b[8m";
+extern RumiColor rumi_color_reset;
+extern RumiColor rumi_color_bright;
+extern RumiColor rumi_color_dim;
+extern RumiColor rumi_color_underscore;
+extern RumiColor rumi_color_blink;
+extern RumiColor rumi_color_reverse;
+extern RumiColor rumi_color_hidden;
 
-RumiColor rumi_color_fg_black = "\x1b[30m";
-RumiColor rumi_color_fg_red = "\x1b[31m";
-RumiColor rumi_color_fg_green = "\x1b[32m";
-RumiColor rumi_color_fg_yellow = "\x1b[33m";
-RumiColor rumi_color_fg_blue = "\x1b[34m";
-RumiColor rumi_color_fg_magenta = "\x1b[35m";
-RumiColor rumi_color_fg_cyan = "\x1b[36m";
-RumiColor rumi_color_fg_white = "\x1b[37m";
+extern RumiColor rumi_color_fg_black;
+extern RumiColor rumi_color_fg_red;
+extern RumiColor rumi_color_fg_green;
+extern RumiColor rumi_color_fg_yellow;
+extern RumiColor rumi_color_fg_blue;
+extern RumiColor rumi_color_fg_magenta;
+extern RumiColor rumi_color_fg_cyan;
+extern RumiColor rumi_color_fg_white;
 
-RumiColor rumi_color_bg_black = "\x1b[40m";
-RumiColor rumi_color_bg_red = "\x1b[41m";
-RumiColor rumi_color_bg_green = "\x1b[42m";
-RumiColor rumi_color_bg_yellow = "\x1b[43m";
-RumiColor rumi_color_bg_blue = "\x1b[44m";
-RumiColor rumi_color_bg_magenta = "\x1b[45m";
-RumiColor rumi_color_bg_cyan = "\x1b[46m";
-RumiColor rumi_color_bg_white = "\x1b[47m";
+extern RumiColor rumi_color_bg_black;
+extern RumiColor rumi_color_bg_red;
+extern RumiColor rumi_color_bg_green;
+extern RumiColor rumi_color_bg_yellow;
+extern RumiColor rumi_color_bg_blue;
+extern RumiColor rumi_color_bg_magenta;
+extern RumiColor rumi_color_bg_cyan;
+extern RumiColor rumi_color_bg_white;
 
 
 
