5057ad695c985a2dfc6c4ee092d3ecee8e9fb755
Ankur Seth <iankurseth@gmail.com>
2020-10-22 16:32:00 +0530

    Keeping the switches away from children

--- lib/rumi_switches.c
+++ lib/rumi_switches.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 #define RUMI_DEFINE_SWITCHES(x) \
-	void rumi_turn_off_ ## x (Rumi rumi) { if (rumi == NULL) return; rumi->show_ ## x = false; } \
-	void rumi_turn_on_  ## x (Rumi rumi) { if (rumi == NULL) return; rumi->show_ ## x =  true; }
+	void rumi_turn_off_ ## x (Rumi rumi) { if (rumi == NULL || rumi->parent != NULL) return; rumi->show_ ## x = false; } \
+	void rumi_turn_on_  ## x (Rumi rumi) { if (rumi == NULL || rumi->parent != NULL) return; rumi->show_ ## x =  true; }
 
 #define RUMI_DEFINE_INDICATORS(x) \
 	bool rumi_ ## x ## _is_off (Rumi rumi) { if (rumi == NULL) return false; return !rumi->show_ ## x; } \
