2f0ec047f783066643a7ac7f03b30ed5fee257a0
Ankur Seth <iankurseth@gmail.com>
2020-10-24 00:08:19 +0530

    Added macros for each of the log funcs

--- include/rumi_macros.h
+++ include/rumi_macros.h
@@ -1,10 +1,20 @@
 #ifndef RUMI_MACROS_H
 #define RUMI_MACROS_H
 
-#define RUMI_NEW(x) Rumi rumi = rumi_new(x);
-#define RUMI_DELETE(x) rumi = rumi_delete(rumi);
+#define RUMI_NEW(x)          Rumi rumi = rumi_new(x)
+#define RUMI_DELETE(x)       rumi = rumi_delete(rumi)
 
-#define RUMI_OK(...) rumi_ok(rumi, __VA_ARGS__)
+#define RUMI_OK(...)         rumi_ok(rumi, __VA_ARGS__)
+#define RUMI_ERROR(...)      rumi_error(rumi, __VA_ARGS__)
+
+#define RUMI_SUGGEST(...)    rumi_suggest(rumi, __VA_ARGS__)
+#define RUMI_TIP(...)        rumi_tip(rumi, __VA_ARGS__)
+#define RUMI_WARN(...)       rumi_warn(rumi, __VA_ARGS__)
+
+#define RUMI_GOOD(...)       rumi_good(rumi, __VA_ARGS__)
+#define RUMI_BAD(...)        rumi_bad(rumi, __VA_ARGS__)
+
+#define RUMI_NATE(...)       rumi_nate(rumi, __VA_ARGS__)
 
 
 
