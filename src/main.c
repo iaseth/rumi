12abed18ccf4fbc07be4501ad84d3e0667ca2ca7
Ankur Seth <iankurseth@gmail.com>
2020-10-23 17:35:51 +0530

    Getting similar running times for ru->ok() and rumi_ok()

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@ void
 rumi_no_macros ()
 {
 	Rumi rumi = rumi_new("rumi_no_macros");
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 1000000; ++i) {
 		if (i % 2 == 0) {
-			ru->ok(rumi, "This is an even number")->ok(rumi, "More ok");
+			ru->ok(rumi, "This is an even number");
 		} else {
-			rumi_error(rumi, "This is odd");
+			ru->error(rumi, "This is odd");
 		}
 	}
 	rumi = rumi_delete(rumi);
